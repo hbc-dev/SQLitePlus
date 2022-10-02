d180d56123e115b24e35fc9d148c5141bd731218
167_dev <74829313+hbc-dev@users.noreply.github.com>
2022-10-02 19:20:01 +0200

    manager config types
    Created ManagerConfig dev types

---
+++ src/ManagerConfig.d.ts
@@ -0,0 +1,76 @@
+type databaseOptions = {
+    createIfNotExists: boolean = true,
+    path: string;
+    close: boolean = false;
+    models: Array<[string, object]> = [];
+    forceLoad: boolean = false;
+}
+
+type databasesKeyOptions = {
+    [key: string]: databaseOptions
+}
+
+type ManagerConfigOptions = {
+    defaultConfig: string;
+    defaultPoint: string;
+    defaultFileStorage: string;
+    databases: databasesKeyOptions
+}
+
+type cloneDatabaseOptions = {
+    name: string;
+    clone: string;
+    force: boolean = false;
+}
+
+export = class ManagerConfig {
+    public defaultPoint: string;
+    public defaultFileStorage: string;
+    
+    constructor(options: ManagerConfigOptions)
+
+    /**
+     * Elige el punto de acceso por defecto
+     * @param option La dirección dentro del manejador
+     */
+    setDefaultPoint(option: string | null): any;
+
+    /**
+     * Elige el punto donde se buscarán los archivos por defecto
+     * @param option El directorio donde buscar
+     */
+    setDefaultFileStorage(option: string | null): any;
+
+    /**
+     * Añade una nueva base de datos a los modelos
+     * @param name El nombre de la base de datos
+     * @param options Las opciones de la base de datos
+     */
+    addDatabase(name: string, options: databaseOptions): any;
+
+    /**
+     * Edita el modelo de una base de datos
+     * @param name El nombre de la base de datos a editar
+     * @param edit El object con las ediciones
+     */
+    editDatabase(name: string, edit: databaseOptions): any;
+
+    /**
+     * Clona un modelo
+     * @param options Las opciones de la función
+     */
+    cloneDatabase(options: cloneDatabaseOptions): any;
+
+    /**
+     * Elimina un modelo
+     * @param name El nombre del modelo
+     */
+    removeDatabase(name: string): any;
+
+    /**
+     * Elimina varios modelos
+     * @param names Los nombres de los modelos
+     */
+    removeDatabases(...names: string[]): any;
+}
+
--- test/config.js
+++ test/config.js
@@ -5,7 +5,6 @@ const config = new ManagerConfig({
   defaultFileStorage: __dirname,
   defaultPoint: "test/db OR test/NONAME_test OR db OR NONAME OR :memory:",
 });
-
 config.addDatabase('db', {
   createIfNotExists: true,
   models: [
@@ -88,12 +87,11 @@ module.exports = config;
   {
     defaultPoint: String, // para establecer la db desde el inicio
     defaultFileStorage: String, // para establecer una ruta por defecto de dónde crear o buscar bases de datos
-    exclude: Array, // para excluir bases de datos del manejador
 
     dbName: {
       createIfNotExists: Boolean, // si quieres crear la db si no existe
       path: String, // la ruta del archivo
-      open: Boolean, // si quieres cerrar o no las transiciones a la base de datos
+      close: Boolean, // si quieres cerrar o no las transiciones a la base de datos
       models: Array, // el modelo que tendrá la base de datos
       forceLoad: Boolean, // si la base debe ser forzada a cargarse dentro del manejador aunque no se referencie
     }
