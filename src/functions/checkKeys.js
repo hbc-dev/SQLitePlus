d97d66b087671facffce33ec3d9a6817867c860f
Gabriel <themarzon@gmail.com>
2022-10-12 18:20:33 -0400

    Update checkKeys.js

--- src/functions/checkKeys.js
+++ src/functions/checkKeys.js
@@ -1,26 +1,43 @@
-const moduleErr = require("../../utils/moduleErr");
+const moduleErr = require('../../utils/moduleErr');
 
-function checkKeys(object, reference) {
-    let referenceKeys = Object.keys(reference);
+function checkKeys (
 
-    for (let key of Object.keys(object)) {
-      let referenceDat = reference[key] ?? "null";
-      let objectDat = object[key] ?? "null"
+    object,
+    reference
+) {
 
-        if (!referenceKeys.includes(key)) throw new moduleErr(`La fila/propiedad "${key}" no existe`)
+    for (const key in object) {
+
+        const referenceDat = reference[key] ?? 'null';
+
+        let objectDat = object[key] ?? 'null';
+
+        if (!(reference in key))
+
+            throw new moduleErr(`La fila/propiedad "${key}" no existe`);
+
         if (
-          typeof referenceDat !== typeof objectDat ||
-          (Array.isArray(referenceDat) && !Array.isArray(objectDat)) ||
-          (!Array.isArray(referenceDat) && Array.isArray(objectDat))
-        )
-          throw new moduleErr(`El tipo a insertar no coincide con el del modelo: ${key}`);
 
-        if (referenceDat && objectDat && typeof objectDat === 'object') {
-            Object.assign(objectDat, checkKeys(objectDat, referenceDat))
-        };
+            Array.isArray(referenceDat)
+         && Array.isArray(objectDat)
+        )
+            throw new moduleErr(`El tipo a insertar no coincide con el del modelo: ${key}`);
+
+        if (
+            
+            referenceDat
+         && typeof objectDat === 'object'
+        )
+
+            objectDat = { 
+
+                ...objectDat,
+
+                checkKeys(objectDat, referenceDat)
+            };
     }
 
-    return Object.assign({}, reference, object);
+    return { reference, object };
 }
 
-module.exports = checkKeys
+module.exports = checkKeys;
