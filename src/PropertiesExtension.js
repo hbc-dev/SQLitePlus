d8da8ff6f364dc32be083a2356d332219e9f6a7b
Gabriel <themarzon@gmail.com>
2022-10-12 18:36:40 -0400

    Update PropertiesExtension.js

--- src/PropertiesExtension.js
+++ src/PropertiesExtension.js
@@ -1,35 +1,49 @@
-const Database = require('better-sqlite3-with-prebuilds');
-//const moduleErr = require('../utils/moduleErr');
+const Database  = require('better-sqlite3-with-prebuilds');
+// const moduleErr = require('../utils/moduleErr');
 
 class Extension extends Database {
+ 
     #path
     #model
     #id
 
-    constructor(file) {
-        super(file)
+    constructor (file) {
+
+        super(file);
 
         this.inFolder = null;
         this.fileName = null;
-    }
+    };
 
     set Path(path) {
+
         this.#path = path;
-    }
+    };
 
     set Model(model) {
+
         this.#model = model;
-    }
+    };
 
     set Id(id) {
+
         this.#id = id;
-    }
+    };
 
-    getPath() { return this.#path }
+    getPath() {
+        
+        return this.#path
+    };
 
-    getModel() { return this.#model }
+    getModel() {
 
-    getId() { return this.#id }
-}
+        return this.#model
+    };
+
+    getId() {
+
+        return this.#id
+    };
+};
 
 module.exports = Extension;
